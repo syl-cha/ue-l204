8bda847ec4ab314983d8e2be6f1ea11d1c95ee19
Sylvain Chambon <chambon.sylvain@gmail.com>
2025-12-05 08:21:41 +0100

    fix: pb numérotation rapport inter

--- docs/reports/mid/UE_L204_Miniprojets_Avancement_Groupe_5.php
+++ docs/reports/mid/UE_L204_Miniprojets_Avancement_Groupe_5.php
@@ -1,3 +1,3 @@
-\contentsline {tcolorbox}{\numberline {1}{\ignorespaces Gestion de la connexion PDO}}{11}{tcb@cnt@php.1}%
-\contentsline {tcolorbox}{\numberline {2}{\ignorespaces Fonction \texttt {setConnecte()}}}{11}{tcb@cnt@php.2}%
-\contentsline {tcolorbox}{\numberline {3}{\ignorespaces Fonction \texttt {isConnecte()}}}{11}{tcb@cnt@php.3}%
+\contentsline {tcolorbox}{\numberline {1}{\ignorespaces Gestion de la connexion PDO}}{13}{tcb@cnt@php.1}%
+\contentsline {tcolorbox}{\numberline {2}{\ignorespaces Fonction \texttt {setConnecte()}}}{13}{tcb@cnt@php.2}%
+\contentsline {tcolorbox}{\numberline {3}{\ignorespaces Fonction \texttt {isConnecte()}}}{13}{tcb@cnt@php.3}%
